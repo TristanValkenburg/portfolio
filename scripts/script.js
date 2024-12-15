d827d630e4e5cd417a79288d4726329fe32be85d
TristanValkenburg <valkenburgtristan@gmail.com>
2024-12-15 10:32:40 +0100

    Acting Site copy

---
+++ scripts/script.js
@@ -0,0 +1,48 @@
+const cursor = new MouseFollower({
+    el: null,
+    container: document.body,
+    className: 'mf-cursor',
+    innerClassName: 'mf-cursor-inner',
+    textClassName: 'mf-cursor-text',
+    mediaClassName: 'mf-cursor-media',
+    mediaBoxClassName: 'mf-cursor-media-box',
+    iconSvgClassName: 'mf-svgsprite',
+    iconSvgNamePrefix: '-',
+    iconSvgSrc: '',
+    dataAttr: 'cursor',
+    hiddenState: '-hidden',
+    textState: '-text',
+    iconState: '-icon',
+    activeState: '-active',
+    mediaState: '-media',
+    stateDetection: {
+        '-pointer': 'a,button',
+        '-hidden': 'iframe'
+    },
+    visible: true,
+    visibleOnState: false,
+    speed: .2,
+    ease: 'expo.out',
+    overwrite: true,
+    skewing: 2,
+    skewingText: 0,
+    skewingIcon: 2,
+    skewingMedia: 2,
+    skewingDelta: 0.001,
+    skewingDeltaMax: 0.15,
+    stickDelta: 0.15,
+    showTimeout: 20,
+    hideOnLeave: true,
+    hideTimeout: 300,
+    hideMediaTimeout: 300
+});
+
+// document.body.style.cursor = 'none';
+
+// const cursorDot = document.getElementById("cursorDot")
+// const cursorOutline = document.getElementById("cursorOutline")
+
+// window.addEventListener("mousemove", function (e) {
+//     const posX = e.clientX;
+//     const posY = e.clientY;
+// });
